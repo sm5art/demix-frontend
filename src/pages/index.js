e25d217e0ed0342bec04fd7f6915b1b06daf527e
Artur Kashperskiy <art@network.ru>
2020-02-21 00:48:47 -0800

    added antd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Button } from 'antd';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -14,6 +15,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Button>
+      wow
+      </Button>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )

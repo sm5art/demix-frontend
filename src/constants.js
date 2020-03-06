6f70c064ed4922a58a61e016a1710f87b05adcfe
Artur K <artur@heali.ai>
2020-03-06 14:46:59 -0800

    changed hhtps to http

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 const ENV = "dev";
-const API_CONFIG = {dev: 'https://localhost/api', prod: 'https://unmix.us/api', nodocker:'http://localhost:5000/api'}
+const API_CONFIG = {dev: 'https://localhost/api', prod: 'http://unmix.us/api', nodocker:'http://localhost:5000/api'}
 export const API_BASE = API_CONFIG[ENV];

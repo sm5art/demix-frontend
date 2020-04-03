1654950470b645b8187a36d08a2dad8edde7190d
Artur K <artur@heali.ai>
2020-04-02 21:19:48 -0700

    added notify feature and pricing

--- src/api.js
+++ src/api.js
@@ -1,31 +1,34 @@
 import { API_BASE } from './constants';
 
-export const getFilesForUser = (token) => {
-    return new Promise((res, rej) => {
-        if(!token) rej();
-        fetch(API_BASE+'/files', {
-            method: 'GET',
-            headers: {token}
-        }).then(response=>response.json()).then(res).catch(rej)
-    });   
-}
+export const getFilesForUser = (token) => new Promise((res, rej) => {
+  if (!token) rej();
+  fetch(`${API_BASE}/files`, {
+    method: 'GET',
+    headers: { token },
+  }).then((response) => response.json()).then(res).catch(rej);
+});
 
-export const meCall = (token) => {
-    return new Promise((res, rej) => {
-        if(!token) rej();
-        fetch(API_BASE+'/me', {
-            method: 'GET',
-            headers: {token}
-        }).then(response=>response.json()).then(res).catch(rej)
-    });   
-}
+export const meCall = (token) => new Promise((res, rej) => {
+  if (!token) rej();
+  fetch(`${API_BASE}/me`, {
+    method: 'GET',
+    headers: { token },
+  }).then((response) => response.json()).then(res).catch(rej);
+});
 
-export const fileCountCall = (token) => {
-    return new Promise((res, rej) => {
-        if(!token) rej();
-        fetch(API_BASE+'/file_count', {
-            method: 'GET',
-            headers: {token}
-        }).then(response=>response.json()).then(res).catch(rej)
-    });   
-}
+export const fileCountCall = (token) => new Promise((res, rej) => {
+  if (!token) rej();
+  fetch(`${API_BASE}/file_count`, {
+    method: 'GET',
+    headers: { token },
+  }).then((response) => response.json()).then(res).catch(rej);
+});
+
+export const notifyCall = (email) => new Promise((res, rej) => {
+  const formData = new FormData();
+  formData.append('email', email);
+  fetch(`${API_BASE}/notify`, {
+    method: 'POST',
+    body: formData,
+  }).then((response) => response.json()).then(res).catch(rej);
+});
--- src/components/PricingTier.js
+++ src/components/PricingTier.js
@@ -1,47 +1,61 @@
 import React from 'react';
-import { Card, Row, Col, Button } from 'antd';
+import {
+  Card, Row, Col, Button,
+} from 'antd';
 import { CheckCircleTwoTone } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
+import { navigate } from 'gatsby';
 
+import ProModal from './ProModal';
+import { switchModal, switchProModal } from '../redux/auth/actions';
 import { green } from '../utils/colors';
 import theme from '../theme';
 
-const Pricing = () => (
+const Pricing = () => {
+  const dispatch = useDispatch();
+  return (
     <>
-        <Row>
-            <Col style={{padding: theme.spacing.medium}} sm={12} xs={24}>
-                <PricingCard title="Starter" price="Free" checks={["2 free uploads", "seperation of vocals and instrumental",]}/>
-            </Col>
-            <Col style={{padding: theme.spacing.medium}} sm={12} xs={24}>
-                <PricingCard title="Premium" price="$0.99 per song" checks={["seperation of vocals and instrumental", "seperation of vocals, drums, bass, and others", "keeps processed files forever"]}/>
-            </Col>
-        </Row>
-        <Row style={{textAlign: 'center', marginBottom: theme.spacing.large}}>
-        <span style={{...theme.fonts.tiny}}>Refund accepted up to 24 hours after purchase.</span>
-        </Row>
+      <ProModal />
+      <Row>
+        <Col style={{ padding: theme.spacing.medium }} sm={12} xs={24}>
+          <PricingCard onClick={() => dispatch(switchModal())} buttonText="Get started" title="Starter" price="Free" checks={['5 free uploads']} />
+        </Col>
+        <Col style={{ padding: theme.spacing.medium }} sm={12} xs={24}>
+          <PricingCard onClick={() => dispatch(switchProModal())} buttonText="Get notified when Pro releases" billed title="Pro" price="$4.99 a month" checks={['unlimited uploads', 'priority in the queue', 'no ads']} />
+        </Col>
+      </Row>
     </>
+  );
+};
+
+const Check = ({ text }) => (
+  <div>
+    <CheckCircleTwoTone twoToneColor={green[7]} />
+    {' '}
+    <span style={{ ...theme.fonts.small, marginLeft: theme.spacing.tiny }}>{text}</span>
+  </div>
 );
 
-const Check = ({text}) => (
-    <div>
-        <CheckCircleTwoTone twoToneColor={green[7]}/> <span style={{...theme.fonts.small, marginLeft: theme.spacing.tiny}}>{text}</span>
+const PricingCard = ({
+  title, price, checks, billed, buttonText, onClick,
+}) => (
+  <Card bodyStyle={{ display: 'flex', flexDirection: 'column', height: '100%' }} style={{ height: '400px', borderRadius: '7px' }}>
+    <div style={{ textAlign: 'center', fontWeight: 500 }}>
+      <div style={{ ...theme.fonts.small }}>{title}</div>
+      <div style={{ ...theme.fonts.medium, marginTop: theme.spacing.medium }}>{price}</div>
+      {billed && <div style={{ ...theme.fonts.small }}>billed monthly</div> }
     </div>
-);
+    <div style={{ marginTop: theme.spacing.medium, textAlign: 'center' }}>
+      { checks && checks.map((e) => <Check text={e} />) }
+    </div>
+    <div style={{ flexGrow: 1 }} />
+    <div style={{ textAlign: 'center' }}>
+      <Button onClick={onClick} type="primary">
+        {buttonText}
+      </Button>
+    </div>
 
-const PricingCard = ({title, price, checks}) => (
-    <Card bodyStyle={{display: 'flex', flexDirection:'column', height: '100%'}} style={{height:'400px', borderRadius: '7px'}}>
-        <div style={{textAlign: 'center', fontWeight: 500}}>
-            <div style={{...theme.fonts.small}}>{title}</div>
-            <div style={{...theme.fonts.medium, marginTop: theme.spacing.medium}}>{price}</div>
-        </div>
-        <div style={{marginTop: theme.spacing.medium, }}>
-                { checks && checks.map(e=><Check text={e}/>) }
-        </div>
-        <div style={{flexGrow: 1}}></div>
-        <div style={{textAlign: 'center'}}>
-            <Button type="primary">Choose {title}</Button>
-        </div>
-        
-    </Card>
+  </Card>
 );
 
 export default Pricing;

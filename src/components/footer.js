32e2ee6983bc62108df3e54d62f3ce02006891f5
Artur K <artur@heali.ai>
2020-04-04 20:57:07 -0700

    changed pricing and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,17 +4,25 @@ import { Row, Col } from 'antd';
 import theme from '../theme';
 
 const Footer = () => (
-    <footer style={{textAlign: 'center', paddingBottom: theme.spacing.medium}}>
-                    <a style={{marginRight: theme.spacing.small}} href="mailto:contactdemix@gmail.com">Contact Us</a>
-                    &copy; Copyright {(new Date()).getFullYear()} unmix
-
-                    <form style={{marginTop: theme.spacing.small}} action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                    <input type="hidden" name="cmd" value="_s-xclick" />
-                    <input type="hidden" name="hosted_button_id" value="73X3RCSX26H9L" />
-                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-                    </form>
-    </footer>
+  <footer style={{ textAlign: 'center', paddingBottom: theme.spacing.medium }}>
+    <a style={{ marginRight: theme.spacing.small }} href="mailto:contactdemix@gmail.com">Contact Us</a>
+    &copy; Copyright
+    {' '}
+    {(new Date()).getFullYear()}
+    {' '}
+    unmix
+    <div>
+      powered by
+      {' '}
+      <a href="https://github.com/deezer/spleeter">spleeter</a>
+    </div>
+    <form style={{ marginTop: theme.spacing.small }} action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <input type="hidden" name="cmd" value="_s-xclick" />
+      <input type="hidden" name="hosted_button_id" value="73X3RCSX26H9L" />
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+      <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+    </form>
+  </footer>
 );
 
 export default Footer;
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -9,7 +9,7 @@ const Jumbotron = () => {
   const dispatch = useDispatch();
   return (
     <div>
-      <h1 style={{ ...theme.fonts.h1, marginTop: theme.spacing.large, marginBottom: theme.spacing.large }}>Separate vocals from music with state of the art AI</h1>
+      <h1 style={{ ...theme.fonts.h1, marginTop: theme.spacing.large, marginBottom: theme.spacing.large }}>Separate stems from music with state of the art AI</h1>
       <Button type="primary" onClick={() => dispatch(switchModal())} style={{ marginBottom: theme.spacing.large, alignSelf: 'center' }} size="large" shape="round">Get started for free</Button>
     </div>
   );
